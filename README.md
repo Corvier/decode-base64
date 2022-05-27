9d0e4368b51a8c4a5d366f83934e865f7ba27c38
Victor Ruiz <victorherrera1316@gmail.com>
2022-05-27 17:55:06 -0600

    Actualización del módulo 2022-05-27. Esta nueva actualización cuenta con un checkbox para poder cargar las credenciales directamente desde las variables de entorno, y decodificarla para comprender como se usa esta nueva función lea la documentación del archivo README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# decode-base64 versión 0.1
+# decode-base64 versión 1.1
 
 ***Este módulo sirve para decodificar cadenas de texto codificadas en base64***
 
@@ -19,6 +19,25 @@
    Características del comando:
        La principal característica es que podemos decodificar y asignar múltiples variables o cadenas
        de texto en base64.
+
+   USO:
+   1. Entrada de datos en base-64:
+       Aquí se debe ingresar la cadena / cadenas codificadas en base-64, si tiene mas de
+       una cadena insertarla de la siguiente manera -> ['cadena-codificada-1','cadena-codificada-2'],
+       tambien puede proporcinar los datos desde una variable entre llaves o mas variables de la
+       siguiente manera -> ['{variable-con-cadena-codificada-1}','{variable-con-cadena-codificada-2}']
+
+   2. Resultados:
+       Aquí debe ingresar las variables donde desea retornar el resultado decodificado de la cadena
+       para esto debe usar escribir las variables sin llaves y separadas por coma, segun el orden de
+       entrada de los datos.
+       Ejemplo: ['entrada-codificada-1','entrada-codificada-2'] -> resultado-entrada-codificada-1,resultado-entrada-codificada-2
+
+   3. Obtener credenciales: NEW OPTION 2020-05-27
+       Marque esta casilla, para obtener las credenciales desde el archivo .env, solamente asegurese de tener
+       las variables decoded_username, decoded_password y opcional decoded_organization en el robot donde este
+       usando este comando.
+
    ```
 
 ## Autor ✒️
--- __init__.py
+++ __init__.py
@@ -23,8 +23,11 @@ Para instalar librerias se debe ingresar por terminal a la carpeta "libs"
     pip install <package> -t .
 
 """
+import os
 from base64 import decodebytes
+from time import sleep
 from colorama import Fore
+from dotenv import load_dotenv
 
 """
     Obtengo el modulo que fue invocado
@@ -34,21 +37,49 @@ module = GetParams("module")
 
 try:
     if module == "decoder":
+        base_path = ''
         data = GetParams("id_input_base64") # data
         variables = GetParams("id_output_base64") # vars
+        load = GetParams("id_get_pms_base64")
+        base_path = GetVar("base_pathP")
+        print(load)
+        if load != False:
+            if base_path != '':
+                cred_path = f"{base_path}credentials/cred.env"
+                # print(cred_path)
+                try:
+                    load_dotenv(cred_path)
+                    user = os.environ.get('user')
+                    pwd = os.environ.get('password')
+                    org = os.environ.get("organization")
 
-        try:
-            data = eval(data)
-            variables =  variables.split(",")
+                    if user != None and pwd != None:
+                        SetVar("decoded_user", decodebytes(user.encode('utf-8')).decode())
+                        SetVar("decoded_password", decodebytes(pwd.encode('utf-8')).decode())
 
-            for i, var in enumerate(variables):
-                SetVar(var, decodebytes(data[i].encode('utf-8')).decode())
+                        if org is not None:
+                            SetVar("decoded_organization", decodebytes(org.encode('utf-8')).decode())
+                    
+                    print(Fore.CYAN + f'\nObteniendo credenciales...')
+                    sleep(1)
+                    print(Fore.GREEN + f'>  Decodificacion existosa\n' + Fore.WHITE)
+
+                except Exception as e:
+                    print(Fore.CYAN + f'\n> Decodificacion incorrecta\n')
+                    raise print(Fore.MAGENTA + f'Error: {e}' + Fore.WHITE)
+        else:
+            try:
+                data = eval(data)
+                variables =  variables.split(",")
+
+                for i, var in enumerate(variables):
+                    SetVar(var, decodebytes(data[i].encode('utf-8')).decode())
+                
+                print(Fore.GREEN + f'\n> Decodificacion exitosa\n' + Fore.WHITE)
             
-            print(Fore.GREEN + f'\n> Decodificacion exitosa\n')
-        
-        except Exception as e:
-            print(Fore.CYAN + f'\n> Decodificacion incorrecta\n')
-            raise print(Fore.MAGENTA + f'Error: {e}')
+            except Exception as e:
+                print(Fore.CYAN + f'\n> Decodificacion incorrecta\n')
+                raise print(Fore.MAGENTA + f'Error: {e}' + Fore.WHITE)
 
 except Exception as e:
     raise print(Fore.MAGENTA + f'Error: {e}')
